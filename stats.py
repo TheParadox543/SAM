1f130a82e5420df8befc04af4b35c6e465b9b5de
TheParadox543 <samuel.alex.koshy@gmail.com>
2022-04-24 17:10:30 +0530

    added numselli bot, fixed minor issues

--- stats.py
+++ stats.py
@@ -141,35 +141,43 @@ class Stats(commands.Cog):
         await ctx.send(embed=embedVar)
     
     @commands.command(aliases=["cs"])
-    async def currentscore(self,ctx,mode:typing.Literal["og", "classic"],page:int=1):
+    async def currentscore(self, ctx,
+            mode:typing.Literal["og", "classic", "numselli"],
+            page:int=1):
         """Shows the streak currentscores"""
         i=(page-1)*10
         msg = ""
         if mode == "og":
             counter_cursor = og_collection.find(
-                {},
+                {"streak":{"$gte":1}},
                 {'name':1,'streak':1,'_id':0}
             ).sort("streak",-1).skip(i).limit(10)
             title_msg = "Current streaks for og counting"
         elif mode == "classic":
             counter_cursor = classic_collection.find(
-                {},
+                {"streak":{"$gte":1}},
                 {'name':1,'streak':1,'_id':0}
             ).sort("streak",-1).skip(i).limit(10)
             title_msg = "Current streaks for classic counting"
-        # elif mode == 3:
+        # elif mode == "abc":
         #     counter_cursor = abc_collection.find(
         #         {},
         #         {'name':1,'streak':1,'_id':0}
         #     ).sort("streak",-1).skip(i).limit(10)
         #     title_msg = "Current streaks for abc counting"
+        elif mode == "numseli":
+            counter_cursor = numselli_collection.find_one(
+                {"streak":{"$gte":1}},
+                {"name":1,"streak":1,"_id":0}
+            ).sort("streak",-1).skip(i).limit(10)
+            title_msg = "Current Streaks for numselli counting"
         else:
             return
         for counter in counter_cursor:
             i+=1
             name = counter.get('name',"Unknown")
-            high = counter.get('high',0)
-            msg += f"{i}. {name} - {high}\n"
+            streak = counter.get('streak',0)
+            msg += f"{i}. {name} - {streak}\n"
         if msg!="":
             embedVar = Embed(title=title_msg,description=msg,color=color_lamuse)
             await ctx.send(embed=embedVar)
@@ -177,7 +185,7 @@ class Stats(commands.Cog):
             return
 
     @discord.slash_command(name="currentscore",guild_ids=servers)
-    async def currentscores(self, ctx,
+    async def slash_currentscore(self, ctx,
             mode:Option(str,
                 description="Leaderboard mode",
                 choices=["og","classic"]),#'abc'
@@ -252,6 +260,29 @@ class Stats(commands.Cog):
         #                 }
         #             )
         #             page = int(counter_num/10)
+        elif mode == "numselli":
+            title_msg = "Current streaks for numselli counting"
+            while msg == "":
+                i = (page - 1) * 10
+                counter_cursor = classic_collection.find(
+                    {'streak':{"$gte":1}},
+                    {'name':1,'streak':1,'_id':0}
+                ).sort("streak",-1).skip(i).limit(10)
+                for counter in counter_cursor:
+                    i+=1
+                    name = counter.get('name',"Unknown")
+                    high = counter.get('high',0)
+                    msg += f"{i}. {name} - {high}\n"
+                if msg=="":
+                    counter_num = classic_collection.count_documents(
+                        {
+                            'streak':
+                            {
+                                '$gte':1
+                            }
+                        }
+                    )
+                    page = int(counter_num/10)
         else:
             return
         embedVar = Embed(title=title_msg,description=msg,color=color_lamuse)
@@ -259,19 +290,21 @@ class Stats(commands.Cog):
         await ctx.respond(embed=embedVar)
 
     @commands.command(aliases=["lb"])
-    async def leaderboard(self,ctx,mode:int=1,page:int=1):
+    async def leaderboard(self, ctx,
+            mode:typing.Literal["og", "classic", "numselli"],
+            page:int=1):
         """Shows the streak highscores"""
         i=(page-1)*10
         msg = ""
-        if mode == 1:
+        if mode == "og":
             counter_cursor = og_collection.find(
-                {},
+                {"streak":{"$gte":1}},
                 {'name':1,'high':1,'_id':0}
             ).sort("high",-1).skip(i).limit(10)
             title_msg = "Highest streaks for og counting"
-        elif mode == 2:
+        elif mode == "classic":
             counter_cursor = classic_collection.find(
-                {},
+                {"streak":{"$gte":1}},
                 {'name':1,'high':1,'_id':0}
             ).sort("high",-1).skip(i).limit(10)
             title_msg = "Highest streaks for classic counting"
@@ -281,6 +314,12 @@ class Stats(commands.Cog):
         #         {'name':1,'high':1,'_id':0}
         #     ).sort("high",-1).skip(i).limit(10)
         #     title_msg = "Highest streaks for abc counting"
+        elif mode == "numseli":
+            counter_cursor = numselli_collection.find_one(
+                {"streak":{"$gte":1}},
+                {"name":1,"streak":1,"_id":0}
+            ).sort("high",-1).skip(i).limit(10)
+            title_msg = "Highest Streaks for numselli counting"
         else:
             return
         for counter in counter_cursor:
@@ -370,6 +409,29 @@ class Stats(commands.Cog):
         #                 }
         #             )
         #             page = int(counter_num/10)
+        elif mode == "numselli":
+            title_msg = "Highest streaks for numselli counting"
+            while msg == "":
+                i = (page - 1) * 10
+                counter_cursor = numselli_collection.find(
+                    {'high':{"$gte":1}},
+                    {'name':1,'high':1,'_id':0}
+                ).sort("high",-1).skip(i).limit(10)
+                for counter in counter_cursor:
+                    i+=1
+                    name = counter.get('name',"Unknown")
+                    high = counter.get('high',0)
+                    msg += f"{i}. {name} - {high}\n"
+                if msg=="":
+                    counter_num = numselli_collection.count_documents(
+                        {
+                            'high':
+                            {
+                                '$gte':1
+                            }
+                        }
+                    )
+                    page = int(counter_num/10)
         else:
             return
         embedVar = Embed(title=title_msg,description=msg,color=color_lamuse)
